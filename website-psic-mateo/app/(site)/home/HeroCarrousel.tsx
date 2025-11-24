fd4c7fc1f0363c3085e4edfc63a5b826b35e5db0
mateomorejon <matemkamelot@hotmail.com>
2025-11-24 15:22:56 -0500

    fix swipper

--- website-psic-mateo/app/(site)/home/HeroCarrousel.tsx
+++ website-psic-mateo/app/(site)/home/HeroCarrousel.tsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "swiper/css/pagination";
-import {Autoplay, EffectFade, Pagination} from "swiper/modules";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
 
 interface HeroCarouselProps {
     images: string[];
